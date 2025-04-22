638912a82414e84955c4b882ee7c3fe07e95d3ff
Bruno Tomaz <tomazcraft@gmail.com>
2025-04-22 08:04:51 -0300

    Foi criado uma condição para reutilizar em mais de um metodo

--- src/services/FuelServices.java
+++ src/services/FuelServices.java
@@ -9,8 +9,21 @@ public class FuelServices {
 		this.pump = pump;
 	}
 	
+	public double lowerAmount(double liter, double amount) {
+		liter = amount;
+	    double value = liter * this.pump.getFuelPrice();
+	    return value;
+	}
+	
 	public void supplyByValue(double value) {
+		double amount = this.pump.getFuelAmount();
 		double liter = value / this.pump.getFuelPrice();
+		
+		if(amount < liter) {
+			liter = amount;
+			value = liter * this.pump.getFuelPrice();
+		}
+		
 		System.out.printf("Combustivel: %s\nValor: %.2f\nLitros %.2f", this.pump.getFuelType(), value, liter);
 	}
 	
